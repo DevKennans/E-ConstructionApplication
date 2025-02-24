693871738b8358110a059508003b46e9105f49ed
DevKennans <devkennans26@gmail.com>
2025-02-24 13:40:40 +0400

    Some methods for Category Controller were done.

--- src/Core/EConstructionApp.Application/Interfaces/Services/Entities/ICategoryService.cs
+++ src/Core/EConstructionApp.Application/Interfaces/Services/Entities/ICategoryService.cs
@@ -1,7 +1,13 @@
-﻿namespace EConstructionApp.Application.Interfaces.Services.Entities
+﻿using EConstructionApp.Domain.Entities;
+
+namespace EConstructionApp.Application.Interfaces.Services.Entities
 {
     public interface ICategoryService
     {
         Task<(bool IsSuccess, string? Message)> InsertAsync(string name);
+
+        Task<(bool isSuccess, string message, IList<Category> categories)> GetAllCategoriesAsync(bool includeDeleted = false);
+
+        Task<(bool isSuccess, string message, IList<Category> categories, int totalCategories)> GetPagedCategoriesAsync(int page = 1, int size = 5, bool includeDeleted = false);
     }
 }
--- src/Infrastructure/EConstructionApp.Persistence/Concretes/Services/Entities/CategoryService.cs
+++ src/Infrastructure/EConstructionApp.Persistence/Concretes/Services/Entities/CategoryService.cs
@@ -29,5 +29,42 @@ namespace EConstructionApp.Persistence.Concretes.Services.Entities
 
             return (true, null);
         }
+
+        public async Task<(bool isSuccess, string message, IList<Category> categories)> GetAllCategoriesAsync(bool includeDeleted = false)
+        {
+            IList<Category> categories = await _unitOfWork.GetReadRepository<Category>()
+                .GetAllAsync(
+                    includeDeleted: includeDeleted,
+                    orderBy: q => q.OrderByDescending(c => c.InsertedDate),
+                    enableTracking: false
+                );
+
+            if (!categories.Any())
+                return (false, "No categories found.", default!);
+
+            return (true, "Categories retrieved successfully.", categories);
+        }
+
+        public async Task<(bool isSuccess, string message, IList<Category> categories, int totalCategories)> GetPagedCategoriesAsync(int page = 1, int size = 5, bool includeDeleted = false)
+        {
+            if (page < 1 || size < 1)
+                return (false, "Page and size must be greater than zero.", default!, 0);
+
+            IList<Category> categories = await _unitOfWork.GetReadRepository<Category>()
+                .GetAllByPagingAsync(
+                    includeDeleted: includeDeleted,
+                    orderBy: q => q.OrderByDescending(c => c.InsertedDate),
+                    enableTracking: false,
+                    currentPage: page,
+                    pageSize: size
+                );
+
+            int totalCategories = await _unitOfWork.GetReadRepository<Category>().CountAsync();
+
+            if (!categories.Any())
+                return (false, "No categories found.", default!, totalCategories);
+
+            return (true, "Categories retrieved successfully.", categories, totalCategories);
+        }
     }
 }
