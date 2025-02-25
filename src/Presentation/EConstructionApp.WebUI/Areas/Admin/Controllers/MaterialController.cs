d27bf188617093f979e6280869e634bfbf276908
Telman13 <teliyev10@std.beu.edu.az>
2025-02-25 16:35:40 +0400

    create is ready for material controller

--- src/Presentation/EConstructionApp.WebUI/Areas/Admin/Controllers/MaterialController.cs
+++ src/Presentation/EConstructionApp.WebUI/Areas/Admin/Controllers/MaterialController.cs
@@ -1,13 +1,67 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EConstructionApp.Application.DTOs.Categories;
+using EConstructionApp.Application.DTOs.Materials;
+using EConstructionApp.Application.Interfaces.Services.Entities;
+using EConstructionApp.Domain.Enums;
+using EConstructionApp.WebUI.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace EConstructionApp.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class MaterialController : Controller
     {
-        public IActionResult AddMaterial()
+        private readonly IMaterialService _materialService;
+        private readonly ICategoryService _categoryService;
+
+        public MaterialController(IMaterialService materialService, ICategoryService categoryService)
+        {
+            _materialService = materialService;
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AddMaterial()
+        {
+            var (isSuccess, message, categories) = await _categoryService.GetAllOrOnlyActiveCategoriesListAsync();
+            if (!isSuccess)
+            {
+                TempData["ErrorMessage"] = "Failed to load categories.";
+            }
+            var model = new CreateMaterialViewModel
+            {
+                Categories = categories ?? new List<CategoryDto>(),
+                Material = new MaterialInsertDto()
+            };
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddMaterial(CreateMaterialViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["ErrorMessage"] = "Please fill in all required fields correctly.";
+            }
+            else
+            {
+                var (isSuccess, message) = await _materialService.InsertAsync(model.Material);
+                var (isSuccessCategories, messageCategories, categories) = await _categoryService.GetAllOrOnlyActiveCategoriesListAsync();
+                model.Categories = categories;
+                if (isSuccess)
+                {
+                    TempData["SuccessMessage"] = message;
+                    return View(model);
+                }
+
+                TempData["ErrorMessage"] = message;
+            }
+            return View(model);
+        }
+        public async Task<IActionResult> GetMaterial()
         {
             return View();
         }
+
+
     }
 }
---
+++ src/Presentation/EConstructionApp.WebUI/Areas/Admin/Models/CreateMaterialViewModel.cs
@@ -0,0 +1,11 @@
+﻿using EConstructionApp.Application.DTOs.Materials;
+using EConstructionApp.Application.DTOs.Categories;
+
+namespace EConstructionApp.WebUI.Areas.Admin.Models
+{
+    public class CreateMaterialViewModel
+    {
+        public MaterialInsertDto Material { get; set; } = new MaterialInsertDto();
+        public IEnumerable<CategoryDto> Categories { get; set; } = new List<CategoryDto>();
+    }
+}
