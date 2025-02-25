85d3894774671f480cadf89827353f8eb049c58a
DevKennans <devkennans26@gmail.com>
2025-02-25 16:39:31 +0400

    Some methods for material service were coded.

---
+++ src/Core/EConstructionApp.Application/DTOs/Materials/MaterialDto.cs
@@ -0,0 +1,22 @@
+﻿using EConstructionApp.Application.DTOs.Categories;
+using EConstructionApp.Domain.Enums;
+
+namespace EConstructionApp.Application.DTOs.Materials
+{
+    public class MaterialDto
+    {
+        public Guid Id { get; set; }
+        public virtual DateTime InsertedDate { get; set; }
+        public virtual DateTime? ModifiedDate { get; set; }
+
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public decimal StockQuantity { get; set; }
+
+        public Measure Measure { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public CategoryDto Category { get; set; }
+    }
+}
--- src/Infrastructure/EConstructionApp.Infrastructure/AutoMapper/Profiles/Material/MaterialProfile.cs
+++ src/Infrastructure/EConstructionApp.Infrastructure/AutoMapper/Profiles/Material/MaterialProfile.cs
@@ -7,10 +7,14 @@ namespace EConstructionApp.Infrastructure.AutoMapper.Profiles.Material
     {
         public MaterialProfile()
         {
+            CreateMap<Domain.Entities.Material, MaterialDto>()
+                .ForMember(dest => dest.Category, opt => opt.MapFrom(src => src.Category));
+
             CreateMap<MaterialInsertDto, Domain.Entities.Material>()
                 .ForMember(dest => dest.Id, opt => opt.Ignore())
                 .ForMember(dest => dest.InsertedDate, opt => opt.Ignore())
-                .ForMember(dest => dest.ModifiedDate, opt => opt.Ignore());
+                .ForMember(dest => dest.ModifiedDate, opt => opt.Ignore())
+                .ForMember(dest => dest.IsDeleted, opt => opt.Ignore());
         }
     }
 }
