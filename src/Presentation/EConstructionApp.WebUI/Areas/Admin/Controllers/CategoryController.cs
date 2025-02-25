6e12a17e41d25da1c590a601f0eb0c44437f424e
Telman13 <teliyev10@std.beu.edu.az>
2025-02-25 12:50:03 +0400

    CategoryController is ready

--- src/Presentation/EConstructionApp.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ src/Presentation/EConstructionApp.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using EConstructionApp.Application.Interfaces.Services.Entities;
+﻿using EConstructionApp.Application.DTOs.Categories;
+using EConstructionApp.Application.Interfaces.Services.Entities;
 using EConstructionApp.WebUI.Areas.Admin.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -32,12 +33,43 @@ namespace EConstructionApp.WebUI.Areas.Admin.Controllers
 
         public async Task<IActionResult> GetCategories(int page = 1, int size = 5)
         {
-            var (isSuccess, message, categories, totalCategories) = await _categoryService.GetPagedCategoriesAsync(page, size);
+            var (isSuccess, message, categories, totalCategories) = await _categoryService.GetAllOrOnlyActiveCategoriesPagedListAsync(page, size);
 
-            if (!isSuccess)
+            if (!isSuccess || categories == null)
             {
                 ViewBag.Error = message;
-                return View();
+                return View(new CategoryListViewModel
+                {
+                    Categories = new List<CategoryDto>(), 
+                    CurrentPage = page,
+                    TotalPages = 0
+                });
+            }
+
+            var totalPages = (int)Math.Ceiling((double)totalCategories / size);
+            var model = new CategoryListViewModel
+            {
+                Categories = categories, 
+                CurrentPage = page,
+                TotalPages = totalPages
+            };
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> GetDeletedCategories(int page = 1, int size = 5)
+        {
+            var (isSuccess, message, categories, totalCategories) = await _categoryService.GetDeletedCategoriesPagedListAsync(page, size);
+
+            if (!isSuccess || categories == null)
+            {
+                ViewBag.Error = message;
+                return View(new CategoryListViewModel
+                {
+                    Categories = new List<CategoryDto>(),
+                    CurrentPage = page,
+                    TotalPages = 0
+                });
             }
             var totalPages = (int)Math.Ceiling((double)totalCategories / size);
             var model = new CategoryListViewModel
@@ -48,6 +80,50 @@ namespace EConstructionApp.WebUI.Areas.Admin.Controllers
             };
 
             return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> RestoreDeletedCategory(Guid categoryId)
+        {
+            var (isSuccess, message) = await _categoryService.RestoreDeletedCategoryAsync(categoryId);
+            if (!isSuccess)
+            {
+                TempData["ErrorMessage"] = message;
+            }
+            else
+            {
+                TempData["SuccessMessage"] = "Category restored successfully.";
+            }
+            return RedirectToAction("GetDeletedCategories");
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditCategory(CategoryDto viewModel)
+        {
+            var (isSuccess, message) = await _categoryService.UpdateCategoryAsync(viewModel.Id, viewModel.Name);
+
+            if (!isSuccess)
+            {
+                TempData["ErrorMessage"] = message; 
+            }
+            else
+            {
+                TempData["SuccessMessage"] = message; 
+            }
+            return RedirectToAction("GetCategories"); 
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteCategory(Guid categoryId)
+        {
+            var (isSuccess, message) = await _categoryService.SafeDeleteCategoryAsync(categoryId);
+
+            if (!isSuccess)
+            {
+                TempData["ErrorMessage"] = message;
+                return RedirectToAction("GetCategories");
+            }
+            TempData["SuccessMessage"] = message;
+            return RedirectToAction("GetCategories");
         }
     }
 }
