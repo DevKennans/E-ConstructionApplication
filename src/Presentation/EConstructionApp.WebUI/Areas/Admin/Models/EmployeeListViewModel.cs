1385e7ac833dbf11493ecb77e30e2f920f0051a3
Telman13 <teliyev10@std.beu.edu.az>
2025-02-26 12:10:06 +0400

    get employees and create employess were done

---
+++ src/Presentation/EConstructionApp.WebUI/Areas/Admin/Models/EmployeeListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using EConstructionApp.Application.DTOs.Categories;
+using EConstructionApp.Application.DTOs.Employees;
+
+namespace EConstructionApp.WebUI.Areas.Admin.Models
+{
+    public class EmployeeListViewModel
+    {
+        public IEnumerable<EmployeeDto> Employees { get; set; } = new List<EmployeeDto>();
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
