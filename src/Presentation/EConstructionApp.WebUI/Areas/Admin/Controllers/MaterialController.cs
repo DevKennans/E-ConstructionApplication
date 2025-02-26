6b97c883965a27fc27e340ad1c8b8daeaa488ffa
Telman13 <teliyev10@std.beu.edu.az>
2025-02-26 10:46:07 +0400

    material controller is ready

--- src/Presentation/EConstructionApp.WebUI/Areas/Admin/Controllers/MaterialController.cs
+++ src/Presentation/EConstructionApp.WebUI/Areas/Admin/Controllers/MaterialController.cs
@@ -5,6 +5,7 @@ using EConstructionApp.Domain.Enums;
 using EConstructionApp.WebUI.Areas.Admin.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Drawing;
 
 namespace EConstructionApp.WebUI.Areas.Admin.Controllers
 {
@@ -57,11 +58,99 @@ namespace EConstructionApp.WebUI.Areas.Admin.Controllers
             }
             return View(model);
         }
-        public async Task<IActionResult> GetMaterial()
+        public async Task<IActionResult> GetMaterial(int page = 1, int size = 5)
         {
-            return View();
+            var (isSuccess, message, materials, totalMaterials) = await _materialService.GetAllOrOnlyActiveMaterialsPagedListAsync(page, size);
+            var (isSuccessCategories, messageCategories, categories) = await _categoryService.GetAllOrOnlyActiveCategoriesListAsync();
+            if (!isSuccess || materials == null)
+            {
+                ViewBag.Error = message;
+                return View(new MaterialListViewModel
+                {
+                    Materials = new List<MaterialDto>(),
+                    CurrentPage = page,
+                    TotalPages = 0,
+                });
+            }
+
+            var totalPages = (int)Math.Ceiling((double)totalMaterials / size);
+            var model = new MaterialListViewModel
+            {
+                Materials = materials,
+                CurrentPage = page,
+                TotalPages = totalPages,
+                Categories = categories
+            };
+
+            return View(model);
+        }
+        public async Task<IActionResult> GetDeletedMaterials(int page = 1, int size = 5)
+        {
+            var (isSuccess, message, materials, totalMaterials) = await _materialService.GetDeletedMaterialsPagedListAsync(page, size);
+
+            if (!isSuccess || materials == null)
+            {
+                TempData["ErrorMessage"] = message;
+                return View(new MaterialListViewModel
+                {
+                    Materials = new List<MaterialDto>(),
+                    CurrentPage = page,
+                    TotalPages = 0
+                });
+            }
+            var totalPages = (int)Math.Ceiling((double)totalMaterials / size);
+            var model = new MaterialListViewModel
+            {
+                Materials = materials,
+                CurrentPage = page,
+                TotalPages = totalPages
+            };
+
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> RestoreDeletedMaterial(Guid materialId)
+        {
+            var (isSuccess, message) = await _materialService.RestoreMaterialAsync(materialId);
+            if (!isSuccess)
+            {
+                TempData["ErrorMessage"] = message;
+            }
+            else
+            {
+                TempData["SuccessMessage"] = message;
+            }
+            return RedirectToAction("GetDeletedMaterials");
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteMaterial(Guid materialId)
+        {
+            var (isSuccess, message) = await _materialService.SafeDeleteMaterialAsync(materialId);
+            if (!isSuccess)
+            {
+                TempData["ErrorMessage"] = message;
+                return RedirectToAction("GetMaterial");
+            }
+            TempData["SuccessMessage"] = message;
+            return RedirectToAction("GetMaterial");
+        }
 
+        [HttpPost]
+        public async Task<IActionResult> EditMaterial(MaterialUpdateDto viewModel)
+        {
+            var (isSuccess, message) = await _materialService.UpdateAsync(viewModel);
+
+            if (!isSuccess)
+            {
+                TempData["ErrorMessage"] = message;
+            }
+            else
+            {
+                TempData["SuccessMessage"] = message;
+            }
+            return RedirectToAction("GetMaterial");
+        }
     }
 }
