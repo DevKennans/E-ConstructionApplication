dbbc0665aea51e965e1495832259b2342f3727a6
Telman13 <teliyev10@std.beu.edu.az>
2025-02-27 18:02:52 +0400

    Employee controller was done

--- src/Presentation/EConstructionApp.WebUI/Areas/Admin/Controllers/EmployeeController.cs
+++ src/Presentation/EConstructionApp.WebUI/Areas/Admin/Controllers/EmployeeController.cs
@@ -1,5 +1,6 @@
 ﻿using EConstructionApp.Application.DTOs.Categories;
 using EConstructionApp.Application.DTOs.Employees;
+using EConstructionApp.Application.DTOs.Materials;
 using EConstructionApp.Application.Interfaces.Services.Entities;
 using EConstructionApp.Persistence.Concretes.Services.Entities;
 using EConstructionApp.WebUI.Areas.Admin.Models;
@@ -24,23 +25,14 @@ namespace EConstructionApp.WebUI.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> AddEmployee(EmployeeInsertDto dto)
         {
-            if (ModelState.IsValid)
+            var (isSuccess, message) = await _employeeService.InsertAsync(dto);
+            if (!isSuccess)
             {
-                var result = await _employeeService.InsertEmployeeAsync(dto);
-
-                if (result.isSuccess)
-                {
-                    TempData["SuccessMessage"] = result.message;
-                    return View();
-                }
-                else
-                {
-                    TempData["ErrorMessage"] = result.message;
-                    return View();
-                }
+                TempData["ErrorMessage"] = message;
+                return View();
             }
-
-            return View(dto); 
+            TempData["SuccessMessage"] = message;
+            return View();
         }
 
         public async Task<IActionResult> GetEmployees(int page = 1, int size = 5)
@@ -49,7 +41,7 @@ namespace EConstructionApp.WebUI.Areas.Admin.Controllers
 
             if (!isSuccess || employees == null)
             {
-                ViewBag.Error = message;
+                TempData["ErrorMessage"] = message;
                 return View(new EmployeeListViewModel
                 {
                     Employees = new List<EmployeeDto>(),
@@ -67,6 +59,74 @@ namespace EConstructionApp.WebUI.Areas.Admin.Controllers
             };
 
             return View(model);
+        }
+
+        public async Task<IActionResult> GetDeletedEmployees(int page = 1, int size = 5)
+        {
+            var (isSuccess, message, employees, totalEmployees) = await _employeeService.GetDeletedEmployeesPagedListAsync(page, size);
+
+            if (!isSuccess || employees == null)
+            {
+                return View(new EmployeeListViewModel
+                {
+                    Employees = new List<EmployeeDto>(),
+                    CurrentPage = page,
+                    TotalPages = 0
+                });
+            }
+            var totalPages = (int)Math.Ceiling((double)totalEmployees / size);
+            var model = new EmployeeListViewModel
+            {
+                Employees = employees,
+                CurrentPage = page,
+                TotalPages = totalPages
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RestoreDeletedEmployee(Guid employeeId)
+        {
+            var (isSuccess, message) = await _employeeService.RestoreEmployeeAsync(employeeId);
+            if (!isSuccess)
+            {
+                TempData["ErrorMessage"] = message;
+            }
+            else
+            {
+                TempData["SuccessMessage"] = message;
+            }
+            return RedirectToAction("GetDeletedEmployees");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteEmployee(Guid employeeId)
+        {
+            var (isSuccess, message) = await _employeeService.SafeDeleteEmployeeAsync(employeeId);
+            if (!isSuccess)
+            {
+                TempData["ErrorMessage"] = message;
+                return RedirectToAction("GetEmployees");
+            }
+            TempData["SuccessMessage"] = message;
+            return RedirectToAction("GetEmployees");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditEmployee(EmployeeUpdateDto viewModel)
+        {
+            var (isSuccess, message) = await _employeeService.UpdateAsync(viewModel);
+
+            if (!isSuccess)
+            {
+                TempData["ErrorMessage"] = message;
+            }
+            else
+            {
+                TempData["SuccessMessage"] = message;
+            }
+            return RedirectToAction("GetEmployees");
         }
     }
 }
