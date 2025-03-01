199266339977b01d958a7b4338bf252fc9a9f171
Telman13 <teliyev10@std.beu.edu.az>
2025-03-01 13:57:05 +0400

    Create Task was done

---
+++ src/Presentation/EConstructionApp.WebUI/Areas/Admin/Models/TaskCreateViewModel.cs
@@ -0,0 +1,20 @@
+﻿using EConstructionApp.Application.DTOs.Employees;
+using EConstructionApp.Application.DTOs.Materials;
+using EConstructionApp.Application.DTOs.Tasks;
+
+namespace EConstructionApp.WebUI.Areas.Admin.Models
+{
+    public class TaskCreateViewModel
+    {
+        public TaskInsertDto Task { get; set; }
+        public IEnumerable<EmployeeDto>? Employees { get; set; }
+        public IEnumerable<MaterialDto>? Materials { get; set; }
+
+        public TaskCreateViewModel()
+        {
+            Task = new TaskInsertDto();
+            Employees = new List<EmployeeDto>();
+            Materials = new List<MaterialDto>();
+        }
+    }
+}
