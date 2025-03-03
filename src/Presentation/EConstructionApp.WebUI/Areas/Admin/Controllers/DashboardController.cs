a9444db1b62ea36497818e298ca4b61446a526ee
Telman13 <teliyev10@std.beu.edu.az>
2025-03-03 10:04:55 +0400

    dashboard component was done

--- src/Presentation/EConstructionApp.WebUI/Areas/Admin/Controllers/DashboardController.cs
+++ src/Presentation/EConstructionApp.WebUI/Areas/Admin/Controllers/DashboardController.cs
@@ -1,4 +1,6 @@
-﻿using EConstructionApp.WebUI.Areas.Admin.Models;
+﻿using EConstructionApp.Application.Interfaces.Services.Entities;
+using EConstructionApp.Persistence.Concretes.Services.Entities;
+using EConstructionApp.WebUI.Areas.Admin.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EConstructionApp.WebUI.Areas.Admin.Controllers
@@ -6,10 +8,61 @@ namespace EConstructionApp.WebUI.Areas.Admin.Controllers
     [Area("Admin")]
     public class DashboardController : Controller
     {
-        [HttpGet]
-        public IActionResult Index()
+        private readonly ICategoryService _categoryService;
+        private readonly IMaterialService _materialService;
+        private readonly IEmployeeService _employeeService;
+        private readonly ITaskService _taskService;
+
+        public DashboardController(ICategoryService categoryService, IMaterialService materialService, IEmployeeService employeeService, ITaskService taskService)
         {
-            return View();
+            _categoryService = categoryService;
+            _materialService = materialService;
+            _employeeService = employeeService;
+            _taskService = taskService;
         }
+        public async Task<IActionResult> Index()
+        {
+            var (categorySuccess, categoryMessage, activeCategories, totalCategories) = await _categoryService.GetCategoryCountsAsync();
+            var (materialSuccess, materialMessage, activeMaterials, totalMaterials) = await _materialService.GetMaterialCountsAsync();
+            var (employeeSuccess, employeeMessage, activeEmployees, totalEmployees) = await _employeeService.GetEmployeeCountsAsync();
+            var (taskSuccess, taskMessage, activeTasks, totalTasks) = await _taskService.GetTaskCountsAsync();
+
+            if (!categorySuccess)
+            {
+                TempData["ErrorMessage"] = categoryMessage;
+            }
+
+            if (!materialSuccess)
+            {
+                TempData["ErrorMessage"] = materialMessage;
+            }
+
+            if (!employeeSuccess)
+            {
+                TempData["ErrorMessage"] = employeeMessage;
+            }
+
+            if (!taskSuccess)
+            {
+                TempData["ErrorMessage"] = taskMessage;
+            }
+
+            var model = new DashboardViewModel
+            {
+                ActiveCategories = activeCategories,
+                TotalCategories = totalCategories,
+                ActiveMaterials = activeMaterials,
+                TotalMaterials = totalMaterials,
+                ActiveEmployees = activeEmployees,
+                TotalEmployees = totalEmployees,
+                ActiveTasks = activeTasks,
+                TotalTasks = totalTasks
+            };
+
+            return View(model);
+        }
+
+
+
     }
 }
---
+++ src/Presentation/EConstructionApp.WebUI/Areas/Admin/Models/DashboardViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace EConstructionApp.WebUI.Areas.Admin.Models
+{
+    public class DashboardViewModel
+    {
+        public int ActiveCategories { get; set; }
+        public int TotalCategories { get; set; }
+        public int ActiveMaterials { get; set; }
+        public int TotalMaterials { get; set; }
+        public int ActiveEmployees { get; set; }
+        public int TotalEmployees { get; set; }
+        public int ActiveTasks { get; set; }
+        public int TotalTasks { get; set; }
+    }
+}
